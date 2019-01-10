4055866a32498e8ce3fe253c9c22352376967bde
Felipe Gomez <fiatdv@icloud.com>
2019-01-10 02:33:37 -0500

    NetworkService 2

---
+++ myWeather/Utils/NetworkService.h
@@ -0,0 +1,32 @@
+//
+//  NetworkService.h
+//  myWeather
+//
+//  Created by Felipe on 1/10/19.
+//  Copyright © 2019 Felipe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class NetworkService;
+
+@protocol NetworkServiceDelegate <NSObject>
+
+- (void)networkServiceDelegate:(NetworkService *)delegate didFinishRequestWithIdentifier:(NSString *)identifier data:(NSData *)data andError:(NSError *)error;
+
+@end
+
+
+@interface NetworkService : NSObject
+
+@property (nonatomic, strong) NSURLSession *session;
+
+@property (nonatomic, weak) id<NetworkServiceDelegate>delegate;
+
+- (void)makeGetRequestTo:(NSURL *)url withIdentifier:(NSString *)identifier withKey:(NSString *)key;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ myWeather/Utils/NetworkService.m
@@ -0,0 +1,87 @@
+//
+//  NetworkService.m
+//  myWeather
+//
+//  Created by Felipe on 1/10/19.
+//  Copyright © 2019 Felipe. All rights reserved.
+//
+
+#import "NetworkService.h"
+#import <UIKit/UIKit.h>
+
+static BOOL sNetworkError = NO;
+
+@implementation NetworkService
+
+#pragma mark Network Requests
+
+- (void)makeGetRequestTo:(NSURL *)url withIdentifier:(NSString *)identifier withKey:(NSString *)key {
+    
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    //[urlRequest setValue:key forHTTPHeaderField:@"x-api-key"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    });
+    NSURLSessionDataTask *task = [_session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        // Catch and handle the error so that it
+        // does not cause the whole app to crash
+        // which may happen if the user loses
+        // a connection to the internet
+        if (error) {
+            showNetworkError();
+            NSLog(@"Network Service: dataTaskWithRequest error: %@", error);
+            // Commented out the return because the network activity indicator will never stop
+            // spinning and the delegate will never be informed that the request finished.
+            //            return;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        });
+        [self.delegate networkServiceDelegate:self didFinishRequestWithIdentifier:identifier data:data andError:error];
+    }];
+    
+    [task resume];
+}
+
+void showNetworkError() {
+    
+    if(!sNetworkError) {
+        
+        sNetworkError = YES;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Network Error"
+                                                                           message:@"\nInternet connection is not available.\n\nTry again later."
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *okAction = [UIAlertAction
+                                       actionWithTitle:@"OK"
+                                       style:UIAlertActionStyleDefault
+                                       handler:^(UIAlertAction *action) {
+                                           [alert dismissViewControllerAnimated:YES completion:nil];
+                                           sNetworkError = NO;
+                                       }];
+            [alert addAction:okAction];
+            
+            UIViewController* topController = topMostController();
+            if(topController)
+                [topController presentViewController:alert animated:YES completion:nil];
+        });
+    }
+}
+
+UIViewController* topMostController() {
+    
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (topController.presentedViewController)
+        topController = topController.presentedViewController;
+    return topController;
+}
+
+@end
